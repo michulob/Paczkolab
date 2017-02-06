b40081a187a1104de9aee83d911be72259b81c19
michulob <vlobocki@gmail.com>
2017-02-06 15:47:24 +0100

    class User before test

--- class/User.php
+++ class/User.php
@@ -23,39 +23,44 @@ class User{
   }
 
   public function getId(){
-
+    return $this->id;
   }
   public function getAddress(){
-
+    return $this->address;
   }
   public function getName(){
-
+    return $this->name;
   }
   public function getSurname(){
-
+    return $this->surname;
   }
   public function getCredits(){
-
+    return $this->credits;
   }
   public function getHashedPassword(){
-
+    return $this->hashedPassword;
   }
 
 
-  public function setAddress(){
-
+  public function setAddress($newAddress){
+    $this->address = $newAddress;
+    return TRUE;
   }
-  public function setName(){
-
+  public function setName($newName){
+    $this->name = $newName;
+    return TRUE;
   }
-  public function setSurname(){
-
+  public function setSurname($newSurame){
+    $this->surname = $newSurame;
+    return TRUE;
   }
-  public function setCredits(){
-
+  public function setCredits($newCredits){
+    $this->credits = $newCredits;
+    return TRUE;
   }
-  public function setHashedPassword(){
-
+  public function setHashedPassword($newPassword){
+    $this->hashedPassword = hash($newPassword, PASSWORD_BCRYPT);
+    return TRUE;
   }
 
   public function loadFromDB($id){
